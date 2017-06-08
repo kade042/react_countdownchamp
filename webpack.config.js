1fa5b48d36f286a3dfb9321eeb06b490783ee523
kade042 <dereje88@gmail.com>
2017-06-07 22:54:34 -0600

    Modified all file structure

---
+++ webpack.config.js
@@ -0,0 +1,34 @@
+var path = require('path');
+var webpack = require('webpack');
+
+module.exports = {
+  devtool: 'cheap-module-eval-source-map',
+  entry: [
+    'webpack-hot-middleware/client',
+    './src/index',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+    publicPath: '/static/',
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+  ],
+  module: {
+    loaders: [{
+      test: /\.js$/,
+      loaders: ['react-hot', 'babel'],
+      include: path.join(__dirname, 'src'),
+    },
+    {
+      test: /\.css$/,
+      loader: 'css-loader',
+      options: {
+        modules: true,
+        localIdentName: '[path][name]__[local]--[hash:base64:5]',
+      },
+    },
+  ],
+ }
+};
