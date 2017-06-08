912ea7899c9c05c64fc91953eef1bdc84f29877f
kade042 <dereje88@gmail.com>
2017-06-07 23:12:14 -0600

    adding static data into App.js

--- src/App.js
+++ src/App.js
@@ -5,7 +5,19 @@ import { render } from 'react-dom';
 class App extends Component {
   render() {
     return (
-      <div>countdown champ</div>
+      <div>
+        <div>Countdown to December 25, 2017</div>
+        <div>
+          <div>14 days</div>
+          <div>30 hours</div>
+          <div>15 minutes</div>
+          <div>20 seconds</div>
+        </div>
+        <div>
+          <input placeholder='new date' />
+          <button>Submit</button>
+        </div>
+      </div>
     );
   }
 }
